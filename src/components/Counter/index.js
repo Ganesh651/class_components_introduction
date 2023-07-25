ba569cc81d1b7d483e8801907a0d638df5c3332c
SwethaGajarla <swethagajarla20@gmail.com>
2023-07-25 11:36:45 +0000

    class component introduction and counter application

---
+++ src/components/Counter/index.js
@@ -0,0 +1,34 @@
+import {Component} from 'react'
+import './index.css'
+
+class Counter extends Component {
+  state = {count: 0}
+
+  onIncrease = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+
+  onDecrease = () => {
+    this.setState(prevState => ({count: prevState.count - 1}))
+  }
+
+  render() {
+    const {count} = this.state
+    return (
+      <div className="container">
+        <h1 className="heading">Counter</h1>
+        <p className="count">{count}</p>
+        <div>
+          <button type="button" className="button" onClick={this.onIncrease}>
+            Increase
+          </button>
+          <button type="button" className="button" onClick={this.onDecrease}>
+            Decrease
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Counter
